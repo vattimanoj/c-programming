bd65853ff979f729568f076720af62c3513d2390
vattimanoj <manojvatti2004@gmail.com>
2024-10-30 15:39:14 +0530

    Update even or odd using switch.c

--- even or odd using switch.c
+++ even or odd using switch.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+void main()
+{
+    int n,t;
+    printf("enter a number");
+    scanf("%d",&n);
+    t=n%2;
+    switch(t)
+    {
+        case 0:printf("the given number %d is even",n);
+        break;
+        case 1:printf(" the given number %d is odd",n);
+        break;
+    }
+    
+}
